41c09bef959b4a33cee40be3a970684633ffd46a
memo1093 <67466350+memo1093@users.noreply.github.com>
2020-10-25 14:47:16 +0300

    Lesson 3:Model Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'ToDoApp';
+  user = 'Mehmet';
+  items= [
+    { description: 'Kahvaltı', action: 'No' },
+    { description: 'Sinema', action: 'No' },
+    { description: 'Spor', action: 'No' },
+    { description: 'Ders', action: 'No' }
+  ];
 }
